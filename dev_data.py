9684418b0f5749acc70ff000f7cc197ce5bb6259
jose <joluna113@gmail.com>
2024-06-12 15:48:51 -0700

    original datasets

---
+++ dev_data.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('Data Engenieer')
+
+st.write('In this app we are going to make a streaming application. The '
+         'stack we are going to use are Python as the main language and Pyspark. '
+         'We will be using SQL as the main database language'
+         '')
